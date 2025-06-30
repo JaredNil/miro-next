ac374a9d5ff99be289bb77022536e81bdd9ad334
JaredN <77531492+JaredNil@users.noreply.github.com>
2025-06-30 17:17:59 +0300

    crud-template

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,21 +1,49 @@
 import {href, Link} from "react-router-dom";
 import {ROUTES} from "@/shared/model/routes.tsx";
 import {rqClient} from "@/shared/api/instance.ts";
+import {useQueryClient} from "@tanstack/react-query";
 
 function BoardsListPage() {
 
+    const queryClient = useQueryClient();
+
+
     const boardQuery = rqClient.useQuery("get",
-        '/boards');
+        '/boards', {
+            onSettled: async () => queryClient.invalidateQueries
+            (rqClient.queryOptions("get", "/boards"))
+        });
+    const createBoardMutation = rqClient.useMutation("post",
+        '/boards' , {
+        onSettled: async () => queryClient.
+        invalidateQueries(rqClient.queryOptions("get", "/boards"))
+        })
+    const deleteBoardMutation = rqClient.useMutation("delete",
+        '/boards/{boardId}' , {
+            onSettled: async () => queryClient
+                .invalidateQueries(rqClient.queryOptions("get", "/boards"))
+        })
 
     return <div>
         <div>
             Board page
         </div>
-        {   boardQuery.data?.map(board => (
-                <Link to={href(ROUTES.BOARD, {boardId: board.id})}>{board.name}</Link>
-            )
-        )
+        {boardQuery.data?.map(board => (
+                <>
+                    <Link to={href(ROUTES.BOARD, {boardId: board.id})}>{board.name}</Link>
+
+                    <button onClick={()=> deleteBoardMutation.mutate({
+                        params: {path: {boardId: board.id}}
+                    })}> удалить </button>
+                </>
+            ))
         }
+        <button onClick={()=> createBoardMutation.mutate(  {
+            "body": {
+                "name":"Sth-name"
+            }})}>добавить
+        </button>
+
     </div>;
 }
 
